81e8debdc69fadf2c2c901e7595202f0a268d3d1
Gil Friedman <FriedmanG>
2022-09-20 12:42:52 -0400

    update

--- JavaFiles/SimpleCalculation.java
+++ JavaFiles/SimpleCalculation.java
@@ -5,6 +5,8 @@ public class SimpleCalculation {
         System.out.println("x = " + x);
         x = x % 5;
         System.out.println("x = " + x);
+        x = x * 2;
+        System.out.println("x = " + x);
         x /= 3;
         System.out.println("x = " + x);
         x += 5;
