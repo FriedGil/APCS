a68038916f5684a23573535d29c47dcb86348502
Gil Friedman <FriedmanG>
2022-09-20 13:23:05 -0400

    bogo sort doesn't work

--- JavaFiles/Sort.java
+++ JavaFiles/Sort.java
@@ -1,15 +1,33 @@
+import java.util.Random;
+
+
+
 public class Sort {
     public static void main(String[] args){
-        int[] arr = {5,2,3,7,1,9,5,34,6,4,7,8,2};
-        arr = bubbleSort(arr);
+        int[] arr = {5,2,3,7,1,9,5,34,6,4,7,8,2,5,19};
         long startTime = System.nanoTime();
-        for (int element:arr) {
-            System.out.println(element);
+        for (int i = 0; i<100000; i++) {
+            arr = bubbleSort(arr);
         }
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         double elapsedTimeInSecond = (double) duration / 1000000000;
-        System.out.println(String.format(("Duration: %f"),elapsedTimeInSecond));
+        for (int element:arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println(String.format(("Bubble Sort Duration: %f"),elapsedTimeInSecond));
+        startTime = System.nanoTime();
+        for (int i = 0; i<100000; i++) {
+            arr = bogoSort(arr);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        elapsedTimeInSecond = (double) duration / 1000000000;
+        for (int element:arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println(String.format(("Quick Sort Duration: %f"),elapsedTimeInSecond));
+
 
 
     }
@@ -27,7 +45,28 @@ public class Sort {
         }
         return inarr;
     }
-    // private static int[] mergeSort(int[] inarr) {
+    private static int[] bogoSort(int[] inarr) {
+		Random rand = new Random();
+        boolean sorted = false;
 
-    // }
+        while (!sorted) {
+            for (int i = 0; i < inarr.length; i++) {
+                int randomIndexToSwap = rand.nextInt(inarr.length);
+                int temp = inarr[randomIndexToSwap];
+                inarr[randomIndexToSwap] = inarr[i];
+                inarr[i] = temp;
+
+            for (int a = 0; a < inarr.length; a++) {
+                if (inarr[0] < inarr[1]) {
+                    sorted = true;
+                }
+                else {
+                    sorted = false;
+                    continue;
+                }
+        }}
+        }
+
+        return inarr;
+    }
 }
---
+++ JavaFiles/Syntax.java
@@ -0,0 +1,5 @@
+public class Syntax {
+    public static void main(String[] args) {
+            
+    }
+}
