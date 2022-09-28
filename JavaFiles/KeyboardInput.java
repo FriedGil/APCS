577a1ceb973961f5a14c55cc1627c945bdcbc741
Gil Friedman <FriedmanG>
2022-09-28 13:21:17 -0400

    hello

---
+++ JavaFiles/KeyboardInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class KeyboardInput {
+    public static void main(String[] args) {
+        averageGrades();
+
+    }
+    static void getName() {
+        Scanner scan = new Scanner(System.in);    
+        System.out.println("Enter name");
+        String name = scan.nextLine(); 
+        System.out.println("Hello " + name);
+    }
+    static void averageGrades() {
+        Scanner scan = new Scanner(System.in);
+        String[] subjects = {"Math", "English", "Science"};
+        int gradeSum = 0;
+        for (String subject:subjects) {
+            System.out.println("Please enter your " + subject + " grade:");
+            gradeSum += scan.nextInt();
+        }
+        double average = (gradeSum / subjects.length);
+        System.out.println("Your average is " + average);
+    }
+}
--- JavaFiles/TypeCasts.java
+++
@@ -1,5 +0,0 @@
-public class TypeCasts {
-    public static void main(String[] args) {
-        
-    }
-}
