40e9451aa9dc1d08963fcff1952a23586e5a57a6
Gil Friedman <FriedmanG>
2022-09-30 13:22:22 -0400

    stringing

--- JavaFiles/Strings.java
+++ JavaFiles/Strings.java
@@ -1,11 +1,28 @@
 public class Strings {
     public static void main(String[] args) {
-        String s1 = "crab";
-        String s2 = new String("crab");
-        String s3 = s1;
-        System.out.println(s3.equals(s2)); //true
-        System.out.println(s1.equals(s2)); //true
-        System.out.println(s1 == s2); //false
+        char[] contents = {'h','e','l','l','o'};
+        char[] contents2 = {'h','e','l','l','o'};
+
+        GilString s1 = new GilString(contents);
+        GilString s2 = new GilString(contents2);
+        System.out.println(s1.equals(s2));
+
     }
 }
 
+class GilString {
+    private final char[] value;
+    public GilString(char[] val) {
+        value = val;
+    }
+    public GilString(String val) {
+        value = val.toCharArray();
+    }
+    public char[] read(){
+        return this.value;
+    }
+    public boolean equals(GilString otherString){
+        return this.read() == otherString.read();
+    }
+
+}
