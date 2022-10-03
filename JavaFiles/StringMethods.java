ea11db2f8f6aecfc9b5a8633886ed540ead0a88d
Gil Friedman <FriedmanG>
2022-10-03 13:07:25 -0400

    Lithub

---
+++ JavaFiles/StringMethods.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        msft();
+    }
+    static void plusCalculator(){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter your math problem");
+    String query = scan.nextLine();
+    int plus_location = query.indexOf("+");
+    String first_arg = query.substring(0,plus_location);
+    String second_arg = query.substring(plus_location+1);
+    double num1 = Double.parseDouble(first_arg);
+    double num2 = Double.parseDouble(second_arg);
+    System.out.println(num1+num2);
+    }
+    static void msft(){
+        String str = "Microsoft";
+        int loc = str.indexOf("soft");
+        String str1 = str.substring(loc);
+        String str2 = str.substring(loc, str.length());
+        System.out.println(str1 + ", " + str2);
+    }
+}
