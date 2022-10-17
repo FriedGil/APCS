9f44d8738dfa8ec1f7c51bd477e7cb0c59b5f9ac
Gil Friedman <FriedmanG>
2022-10-17 12:54:49 -0400

    down bulk

---
+++ JavaFiles/Circuit.java
@@ -0,0 +1,6 @@
+public class Circuit {
+    public static void main(String[] args){
+    boolean result = 5/0 == 0 && 0!=0;
+    System.out.println(result);
+    }
+}
--- JavaFiles/MrBool.java
+++
@@ -1,7 +0,0 @@
-public class MrBool {
-    MyMath myMath;
-    public static void main(String[] args) {
-        int num = MyMath.randInt(0,5);
-        System.out.println(num);
-    }
-}
