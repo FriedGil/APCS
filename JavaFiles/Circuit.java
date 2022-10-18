b40a8fd1c783d9aba65b26b017070ecffe8485d0
Gil Friedman <FriedmanG>
2022-10-18 12:47:07 -0400

    yo

--- JavaFiles/Circuit.java
+++ JavaFiles/Circuit.java
@@ -1,6 +1,12 @@
 public class Circuit {
     public static void main(String[] args){
-    boolean result = 5/0 == 0 && 0!=0;
-    System.out.println(result);
+    try {
+        boolean result = 3==3 && 3/0 == 0;
+        System.out.println(result);
+
+
+    } catch (Exception e) {
+        System.out.println(e);
+    }
     }
 }
