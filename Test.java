946fc8ace11e6c00bad3703b50bfa5f4c73f6c8d
Gil Friedman <FriedmanG>
2023-04-26 13:19:10 -0400

    Vector operation optimization

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+    public static void main(String[] args) {
+
+        int i = 0, n = 100;
+        for (i++; i<n; n--){
+            System.out.print("here");
+            i++;
+            System.out.println(i);
+        }
+
+    }
+}
